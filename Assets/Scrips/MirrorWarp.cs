55286bb7a9751829f9f267ff5855bb0747499ff0
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-26 17:12:54 +0900

    <fix>出現する座標をずらして無限ループをしないように修正

--- Assets/Scrips/MirrorWarp.cs
+++ Assets/Scrips/MirrorWarp.cs
@@ -1,7 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MirrorWarp : MonoBehaviour
 {
-    [SerializeField] private GameObject mirror1;
-    [SerializeField] private GameObject mirror2;
+    [SerializeField] private Transform anotherMirrorPos;
+    private bool inMirror;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (inMirror == false)
+        {
+            Debug.Log("ì¸Ç¡ÇƒÇ´ÇΩ");
+            var x = anotherMirrorPos.position.x;
+            var y = anotherMirrorPos.position.y;
+            var z = anotherMirrorPos.position.z + 2f;
+            other.transform.position = new Vector3(x, y, z);
+            inMirror = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inMirror = false;
+        Debug.Log("î≤ÇØèoÇµÇΩ");
+    }
 }
