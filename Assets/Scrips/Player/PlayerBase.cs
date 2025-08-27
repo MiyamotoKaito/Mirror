00570bfe02e7aae1fd0c9f3abb279dc810273d47
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-27 17:31:49 +0900

    <update>AwakeからBaseAwake関数に変更

--- Assets/Scrips/Player/PlayerBase.cs
+++ Assets/Scrips/Player/PlayerBase.cs
@@ -3,9 +3,11 @@
 public class PlayerBase : MonoBehaviour
 {
     protected InputSystem_Actions _playerBase;
+    protected Rigidbody _rb;
 
-    protected void Awake()
+    protected void BaseAwake()
     {
+        _rb = GetComponent<Rigidbody>();
         _playerBase = new InputSystem_Actions();
         _playerBase.Enable();
     }
