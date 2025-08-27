0e3c0d33eb179a175ae02b87d67ff6efd7e2232e
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-27 15:32:00 +0900

    <add>プレイヤーの基底クラス

---
+++ Assets/Scrips/Player/PlayerBase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    protected PlayerBase _playerBase;
+
+    protected void Awake()
+    {
+        _playerBase = new PlayerBase();
+        _playerBase.enabled = true;
+    }
+}
