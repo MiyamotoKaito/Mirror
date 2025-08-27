87e32ee5169946d84d948f22c6cb8b0edd901a29
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-27 17:31:02 +0900

    <add>プレイヤーの移動に関するスクリプト

---
+++ Assets/Scrips/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : PlayerBase
+{
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+
+    [Header("Jump")]
+    [SerializeField] private float reCastTime;
+    private float jumptime = 0f;
+    private bool isGround;
+
+    void Awake()
+    {
+        base.BaseAwake();
+    }
+
+    void FixedUpdate()
+    {
+        jumptime += Time.deltaTime;
+
+        //移動処理
+        if (_playerBase.Player.Move.triggered)
+        {
+            Vector2 move = _playerBase.Player.Move.ReadValue<Vector2>();
+            _rb.linearVelocity = new Vector3(move.x, 0f, move.y) * moveSpeed;
+        }
+        //ジャンプ処理
+        else if (_playerBase.Player.Jump.triggered && jumptime > reCastTime)
+        {
+            _rb.linearVelocity = new Vector3(0f, jumpForce, 0f);
+            jumptime = 0f;
+        }
+    }
+}
