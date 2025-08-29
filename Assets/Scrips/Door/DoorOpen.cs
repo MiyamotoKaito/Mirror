0a9843088f2697cac816a2e72ecca5eef237e5e8
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-29 16:36:53 +0900

    <add>ドアを開けるのは一回だけにした

--- Assets/Scrips/Door/DoorOpen.cs
+++ Assets/Scrips/Door/DoorOpen.cs
@@ -2,18 +2,24 @@
 using DG.Tweening;
 
 /// <summary>
-/// ドアを開けるためのクラスDoorSwitchクラスのイベントで呼び出す
+/// ドアを開けるためのクラス
+/// DoorSwitchクラスのイベントで呼び出す
 /// </summary>
 public class DoorOpen : MonoBehaviour
 {
     [SerializeField] private Transform right;
     [SerializeField] private Transform left;
+    private bool isOpen;
 
     public void OpenTheDoor()
     {
-        right.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
-            .OnComplete(() => { right.DOLocalMove(new Vector3(0, 0, -2), 1f).SetRelative(true); });
-        left.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
-            .OnComplete(() => { left.DOLocalMove(new Vector3(0, 0, 2), 1f).SetRelative(true); });
+        if (!isOpen)
+        {
+            isOpen = true;
+            right.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
+                .OnComplete(() => { right.DOLocalMove(new Vector3(0, 0, -2), 1f).SetRelative(true); });
+            left.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
+                .OnComplete(() => { left.DOLocalMove(new Vector3(0, 0, 2), 1f).SetRelative(true); });
+        }
     }
 }
