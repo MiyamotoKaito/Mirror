ef3e75ba0b50379da691918a4d04a97ec0d48cd3
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-29 21:47:03 +0900

    <fix>OnCompleteを閉じれていなかった

--- Assets/Scrips/Door/DoorOpen.cs
+++ Assets/Scrips/Door/DoorOpen.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using DG.Tweening;
+﻿using DG.Tweening;
+using UnityEngine;
 
 /// <summary>
 /// ドアを開けるためのクラス
@@ -9,17 +9,40 @@ public class DoorOpen : MonoBehaviour
 {
     [SerializeField] private Transform right;
     [SerializeField] private Transform left;
+    [SerializeField] private GameObject doorCamera;
+    private GameObject playerCam;
     private bool isOpen;
 
+
+    private void Awake()
+    {
+        doorCamera.SetActive(false);
+        playerCam = GameObject.Find("PlayerCamera");
+    }
     public void OpenTheDoor()
     {
         if (!isOpen)
         {
             isOpen = true;
-            right.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
-                .OnComplete(() => { right.DOLocalMove(new Vector3(0, 0, -2), 1f).SetRelative(true); });
-            left.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
-                .OnComplete(() => { left.DOLocalMove(new Vector3(0, 0, 2), 1f).SetRelative(true); });
+            playerCam.SetActive(false);
+            doorCamera.SetActive(true);
+
+
+            Tween r = right.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
+                 .OnComplete(() =>
+                 {
+                     right.DOLocalMove(new Vector3(0, 0, -2), 1f).SetRelative(true)
+                 .OnComplete(() => { doorCamera.SetActive(false); });
+                 });
+            Tween l = left.DOLocalMove(new Vector3(-0.2f, 0, 0), 1f).SetRelative(true)
+                 .OnComplete(() =>
+                 {
+                     left.DOLocalMove(new Vector3(0, 0, 2), 1f).SetRelative(true)
+                 .OnComplete(() => { playerCam.SetActive(true); });
+                 });
+
+            r.SetDelay(2f);
+            l.SetDelay(2f);
         }
     }
 }
