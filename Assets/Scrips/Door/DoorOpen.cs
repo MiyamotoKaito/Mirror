b5688d98ca6500ae3fad44b66f735d238977e235
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-29 14:58:24 +0900

    <add>ドアを開けるモーションをDOTweenで実装

--- Assets/Scrips/Door/DoorOpen.cs
+++ Assets/Scrips/Door/DoorOpen.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class DoorOpen : MonoBehaviour
 {
     [SerializeField] private Transform right;
     [SerializeField] private Transform left;
-    void Update()
+
+    public void OpenTheDoor()
     {
-        
+        right.DOMoveX(-2, 1f);
+        left.DOMoveX(-2, 1f);
+
+        Tween r = right.DOMoveZ(-2, 1f);
+        Tween l = left.DOMoveZ(2, 1f);
+
+        r.SetDelay(1f);
+        l.SetDelay(1f);
+
     }
 }
