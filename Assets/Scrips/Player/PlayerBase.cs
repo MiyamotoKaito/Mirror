dd1401bde8eb61f609ef9c64fb5f46b062052ca1
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-08-29 15:22:21 +0900

    <delee>RBを使わないスクリプトも派生クラスで使うので基地クラスからの削除

--- Assets/Scrips/Player/PlayerBase.cs
+++ Assets/Scrips/Player/PlayerBase.cs
@@ -3,11 +3,8 @@
 public class PlayerBase : MonoBehaviour
 {
     protected InputSystem_Actions _playerBase;
-    protected Rigidbody _rb;
-
     protected void BaseAwake()
     {
-        _rb = GetComponent<Rigidbody>();
         _playerBase = new InputSystem_Actions();
         _playerBase.Enable();
     }
