c8893cd29a1b0fd942f99d0f2b814529a75542b4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-01 01:26:38 +0900

    <update>鏡の反射先のベクトルを反射用のカメラに加算

--- Assets/Scrips/Mirror/SpecularReflection.cs
+++ Assets/Scrips/Mirror/SpecularReflection.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using UnityEngine;
 
-[ExecuteAlways]
 public class SpecularReflection : MonoBehaviour
 {
     [Tooltip("playerのカメラ"), SerializeField]
-    private Camera playerCamera;
+    private Transform playerCamera;
     [Tooltip("反射先を写すカメラ"), SerializeField]
-    private Camera reflectionCamera;
+    private Transform reflectionCamera;
 
     public static event Action OnMirorUpdate;
 
@@ -32,16 +31,17 @@ public class SpecularReflection : MonoBehaviour
         //反射ベクトルの計算
         #region
         //カメラから鏡面への方向ベクトル
-        var incident = (reflectionCamera.transform.position - playerCamera.transform.position);
+        var incident =(reflectionCamera.position - playerCamera.position);
 
         //鏡面からの法線ベクトル
-        var normal = reflectionCamera.transform.forward;
+        var normal = transform.forward;
 
         //カメラの反射ベクトル
+        //Reflect関数っていうのがあるらしい
         var reflection = (incident + 2 * (Vector3.Dot(-incident, normal)) * normal);
         #endregion
 
         //反射ベクトルの方向に鏡面のカメラを向かせる
-        reflectionCamera.transform.LookAt(reflection);
+        reflectionCamera.LookAt(reflectionCamera.position + reflection);
     }
 }
