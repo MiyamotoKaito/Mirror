1e80e82d5ee4bb99351aecac5913580704df2e71
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-01 06:09:33 +0900

    <add>スケール感を統一させる

--- Assets/Scrips/Mirror/Incident.cs
+++ Assets/Scrips/Mirror/Incident.cs
@@ -4,10 +4,14 @@ public class Incident : MirrorController
 {
     public override void UpdateMirror()
     {
-        //鏡面からカメラへの方向ベクトル
+        //カメラから鏡面への方向ベクトル
         var incident = playerCamera.transform.forward;
 
         //プレイヤーと同じ方向を向かせる
         reflectionCamera.transform.LookAt(reflectionCamera.transform.position + incident);
+
+        //鏡のスケール感を統一
+        reflectionCamera.fieldOfView = playerCamera.fieldOfView;
+        reflectionCamera.nearClipPlane = playerCamera.nearClipPlane;
     }
 }
