d382c99f0fa8eb9492451114d0dc44525f6aa786
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-01 06:05:12 +0900

    <add>ワープ先の鏡の向く方向を制御する

---
+++ Assets/Scrips/Mirror/Incident.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Incident : MirrorController
+{
+    public override void UpdateMirror()
+    {
+        //鏡面からカメラへの方向ベクトル
+        var incident = playerCamera.transform.forward;
+
+        //プレイヤーと同じ方向を向かせる
+        reflectionCamera.transform.LookAt(reflectionCamera.transform.position + incident);
+    }
+}
