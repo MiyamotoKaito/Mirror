4b2b0bf9a3c5b923bb23e028e0ecab33d56e475d
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-01 17:44:50 +0900

    <add>BGMのスクリプト

---
+++ Assets/Scrips/BGM.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void Awake()
+    {
+        if (target == null)
+        {
+            Instantiate(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Update()
+    {
+        transform.position = target.position;
+    }
+}
