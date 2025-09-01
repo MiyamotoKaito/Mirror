fa54d32125ac54da88081227877a495fe25939a1
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-01 22:02:16 +0900

    <add>鏡を軸としたカメラの移動

---
+++ Assets/Scrips/RotateMirror.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+public class RotateMirror : MonoBehaviour
+{
+    [SerializeField] private Vector3 axis;
+    [SerializeField] private GameObject titleMirror;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private CinemachineCamera titleCam;
+    private void FixedUpdate()
+    {
+       titleCam.transform.RotateAround(titleMirror.transform.position, axis, rotateSpeed);
+    }
+}
