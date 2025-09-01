de1de3ec7c147bbcb8f8bb088dc6ebe83c235d19
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-01 22:02:39 +0900

    <add>移動した後のスポットライトのアニメーション

--- Assets/Scrips/TitleAnim.cs
+++ Assets/Scrips/TitleAnim.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
-using DG.Tweening;
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class TitleAnim : MonoBehaviour
 {
-    
+    [SerializeField] private List<GameObject> spotLights;
+    [SerializeField] private Canvas canvas;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        foreach (var light in spotLights)
+        {
+            light.SetActive(false);
+        }
+    }
     void Start()
     {
         
@@ -11,6 +24,16 @@ public class TitleAnim : MonoBehaviour
 
     void Update()
     {
-        
+
+    }
+
+    private IEnumerator LightUP()
+    {
+        for (int i = 0; i < spotLights.Count; i++)
+        {
+            spotLights[i].SetActive(true);
+            AudioManager.Instance.PlaySE("点灯", _audioSource);
+           yield return new WaitForSeconds(0.5f);
+        }
     }
 }
