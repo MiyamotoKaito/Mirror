985d871561ee80e531331ba63dd7b0180f432ecf
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-09-08 00:54:12 +0900

    <add>ファンを回すだけのコンポーネント

---
+++ Assets/Scrips/FanController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FanController : MonoBehaviour
+{
+    [SerializeField] private Transform fan;
+    [SerializeField] private float rotateZ;
+    private AudioSource _fanSource;
+    bool _isStop;
+    void Start()
+    {
+        _fanSource = GetComponent<AudioSource>();
+        AudioManager.Instance.PlayBGM("ファンの音", _fanSource);
+    }
+    void Update()
+    {
+        fan.Rotate(0, 0, rotateZ * Time.deltaTime);
+        if (_isStop)
+        {
+            AudioManager.Instance.SmallerBGM(_fanSource);
+            fan.Rotate(0, 0, rotateZ - Time.deltaTime);
+            if (rotateZ == 0)
+            {
+                AudioManager.Instance.Stop(_fanSource);
+                rotateZ = 0;
+            }
+        }
+    }
+}
