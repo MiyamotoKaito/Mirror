90c5972587c9a49275e217f5522c95504730d271
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-09-09 20:19:43 +0900

    <add>鏡の判定管理クラス

---
+++ Assets/Scrips/Door/DoorCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 鏡を見たときにイベントでドアを無効化するクラス
+/// </summary>
+public class DoorCollider : MonoBehaviour
+{
+    private Collider _door;
+    private void Awake()
+    {
+        _door = GetComponent<Collider>();
+    }
+    /// <summary>
+    /// ドアのコライダーを無効化するメソッド
+    /// </summary>
+    public void ColliderDisActive()
+    {
+        //鏡のコライダーを無効化して通れるようにする
+        _door.enabled = false;
+    }
+    /// <summary>
+    /// ドアそのものを消す
+    /// </summary>
+    public void ObjectDisActive()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
