2519129f5b11326a5b1e3e4cd18642991ecf9655
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-09-09 16:57:25 +0900

    <add>テクスチャのメモリ管理

---
+++ Assets/Scrips/Mirror/NearByMirror.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor.AssetImporters;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NearByMirror : MonoBehaviour
+{
+    /// <summary>鏡</summary>
+    [SerializeField] private GameObject mirror;
+    /// <summary>鏡についてるレンダラーテクスチャ</summary>
+    [SerializeField] private RenderTexture mirrorTex;
+
+    private RenderTexture sourceTex;
+    private void Awake()
+    {
+        sourceTex = mirrorTex;
+        mirror.SetActive(false);
+        mirrorTex.Release();//メモリ解放
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        mirror.SetActive(true);
+        Graphics.Blit(sourceTex, mirrorTex);//書き込み
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        mirror.SetActive(false);
+        mirrorTex.Release();
+    }
+}
