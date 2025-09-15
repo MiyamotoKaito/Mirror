8122b0d87dbadd1451a69329d1a3bb32134ac1e2
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2025-09-15 20:20:52 +0900

    [add]星の所持数管理

---
+++ Assets/Scrips/Star.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        StarManager.instance.ActiveCollectStar();
+    }
+    public void GetStar()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
