28d6b836b8999ab63c2214cb155081ffc9275103
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-16 07:22:55 +0900

    [add]反転ステージに入ったらカメラの操作を反転させるスクリプト

---
+++ Assets/Scrips/Player/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+/// <summary>
+/// カメラの動きを制御するクラス
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    private CinemachineInputAxisController _cameraAxis;
+    private GameObject _camera;
+    private void Awake()
+    {
+        _camera = GameObject.FindGameObjectWithTag("Camera");
+        _cameraAxis = _camera.GetComponent<CinemachineInputAxisController>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Reflection"))
+        {
+            ChangeGain();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Reflection"))
+        {
+            ChangeGain();
+        }
+    }
+    /// <summary>
+    /// 視点操作を反転させるメソッド
+    /// </summary>
+    private void ChangeGain()
+    {
+        //cinemachineInputAxisControllerから軸を取得
+        var controllers = _cameraAxis.Controllers;
+
+        foreach (var controller in controllers)
+        {
+            Debug.Log("反転");
+            controller.Input.Gain *= -1;
+        }
+    }
+}
