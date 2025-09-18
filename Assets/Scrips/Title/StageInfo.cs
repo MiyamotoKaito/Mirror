0a205e35daa50d0432fa8b4f6bba37f5f0629b99
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:28:51 +0900

    [add]TitleSceneLoaderの配列の中身を参照してUIに写すスクリプト

---
+++ Assets/Scrips/Title/StageInfo.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class StageInfo : MonoBehaviour
+{
+    [SerializeField, Header("名前のテキスト")]
+    private TextMeshProUGUI stagNameText;
+    [SerializeField, Header("説明のテキスト")]
+    private TextMeshProUGUI stageInfoText;
+    [SerializeField, Header("ステージのリスト")]
+    private TitleSceneLoader sceneLoader;
+    private void Update()
+    {
+        ShowText();
+    }
+    /// <summary>
+    /// UI上にステージのテキストを表示させる
+    /// </summary>
+    public void ShowText()
+    {
+        //基本情報を表示
+        var camsInfo = sceneLoader.GetCamsInfo();
+        if (camsInfo != null)
+        {
+            stagNameText.enabled = true;
+            stageInfoText.enabled = true;
+            stagNameText.text = camsInfo.SceneName;
+            stageInfoText.text = camsInfo.SceneInfo;
+        }
+        else
+        {
+            HideText();
+        }
+    }
+    /// <summary>
+    /// 全てのテキストを非表示
+    /// </summary>
+    private void HideText()
+    {
+        stagNameText.enabled = false;
+        stageInfoText.enabled = false;
+    }
+}
