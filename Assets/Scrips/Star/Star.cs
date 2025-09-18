c64dc8fad032f94a65d4c71b6428d5701bab2a53
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:16:38 +0900

    [add]星を入手したときのスクリプト

---
+++ Assets/Scrips/Star/Star.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private StarManager _starManager;
+    private void Awake()
+    {
+        var manager = GameObject.Find("StarManager");
+        _starManager = manager.GetComponent<StarManager>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
+        AudioManager.Instance.PlayBGM("星", _audioSource);
+    }
+    public void GetStar()
+    {
+        this.gameObject.SetActive(false);
+        _starManager.ActiveCollectStar();
+    }
+}
