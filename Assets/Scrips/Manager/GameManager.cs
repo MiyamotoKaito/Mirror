f799911c93a10223fad7102e14fc50d2234a2800
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:27:11 +0900

    [add]GameManagerスクリプト

---
+++ Assets/Scrips/Manager/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private string bgmName;
+    private AudioSource _audioSource;
+    private void Start()
+    {
+        _audioSource = AudioManager.Instance.gameObject.GetComponent<AudioSource>();
+        AudioManager.Instance.PlayBGM(bgmName, _audioSource);
+    }
+}
