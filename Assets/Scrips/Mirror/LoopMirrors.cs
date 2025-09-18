47d12c12178bcb70553a5dd8f82173a7338d673a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:27:42 +0900

    [add]ステージの鏡のローテーション管理のスクリプト

---
+++ Assets/Scrips/Mirror/LoopMirrors.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LoopMirrors : MonoBehaviour
+{
+    [SerializeField] private List<Transform> mirrors;
+    [Header("ループの速さ")]
+    [SerializeField] private float delay = 1f;
+    private void Awake()
+    {
+        StartCoroutine(LoopMirror());
+    }
+    /// <summary>
+    /// 鏡のポジションを降下順に入れ替えていく
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LoopMirror()
+    {
+        while (true)//無限ループはするが一回ごとに待機
+        {
+            //最初の鏡
+            var first = mirrors[0].position;
+            for (int i = 0; i < mirrors.Count - 1; i++)
+            {
+                mirrors[i].position = mirrors[i + 1].position;
+            }
+            //最後に先頭に戻す
+            mirrors[mirrors.Count - 1].position = first;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+}
