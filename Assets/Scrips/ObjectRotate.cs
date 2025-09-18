31d9c4b013edfe0d4c81bf1e40e5c9f4b321d235
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:14:51 +0900

    [add]オブジェクトを回転させる基底クラス

---
+++ Assets/Scrips/ObjectRotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour
+{
+    /// <summary>回転させる軸</summary>
+    [SerializeField] protected Vector3 axis;
+    /// <summary>軸となるオブジェクト</summary>
+    [SerializeField] protected GameObject axisObject;
+}
