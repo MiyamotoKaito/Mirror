4a629e8d7e3277e6f5c49b00b5ed36bebd1d8c87
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:26:46 +0900

    [add]インプットシステムの有効化、無効化を管理

--- Assets/Scrips/Title/ModeSelect.cs
+++ Assets/Scrips/Title/ModeSelect.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using Mono.Cecil;
 
 /// <summary>
 /// カメラの位置を切り替えるクラス
@@ -11,11 +12,14 @@ public class ModeSelect : PlayerBase
     /// <summary>シーンがついているカメラ</summary>
     [SerializeField] private List<CinemachineCamera> Cams;
     private int cameraCount = 0;//最初のカメラの優先度
-
+    private AudioSource _audiosource;
     private void Awake()
     {
         base.BaseAwake();
         Cams[0].Priority = 1;//最初のカメラの優先度を1にして他のカメラの衝突を避ける
+        EnableInput();
+        var player = GameObject.Find("SecondCam");
+        _audiosource = player.GetComponent<AudioSource>();
     }
     private void OnEnable()
     {
@@ -36,6 +40,7 @@ public class ModeSelect : PlayerBase
     {
         if (context.started)
         {
+            AudioManager.Instance.PlaySE("視点遷移", _audiosource);
             //現在のカメラの優先度を下げる
             Cams[cameraCount].Priority -= 1;
             cameraCount++;
@@ -58,6 +63,7 @@ public class ModeSelect : PlayerBase
     {
         if (context.started)
         {
+            AudioManager.Instance.PlaySE("視点遷移", _audiosource);
             //現在のカメラの優先度を下げる
             Cams[cameraCount].Priority -= 1;
             cameraCount--;
@@ -71,5 +77,15 @@ public class ModeSelect : PlayerBase
             //次のカメラの優先度を上げる
             Cams[cameraCount].Priority += 1;
         }
+    }
+    /// <summary>
+    /// ゲームから戻ると有効化するため
+    /// </summary>
+    private void EnableInput()
+    {
+        //キーボードを有効化
+        InputSystem.EnableDevice(Keyboard.current);
+        //パッドを有効化
+        InputSystem.EnableDevice(Gamepad.current);
     }
 }
