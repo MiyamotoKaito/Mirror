422eab47394333120c6b4280e87475fb2be78e48
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-09-19 04:28:04 +0900

    [add]星を回すスクリプト

---
+++ Assets/Scrips/Star/StarRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+/// <summary>
+/// 星を回転させるクラス
+/// </summary>
+public class StarRotate : ObjectRotate
+{
+    /// <summary>周るスピード</summary>
+    [SerializeField] private float rotateSpeed;
+    private void Awake()
+    {
+        axisObject = this.gameObject;
+    }
+    private void FixedUpdate()
+    {
+        transform.Rotate(axis * rotateSpeed * Time.fixedDeltaTime);
+    }
+}
